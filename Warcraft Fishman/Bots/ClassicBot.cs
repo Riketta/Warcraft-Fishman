6d4d4dcaf9b6070393402353c74021f22eafd947
Eugene "Riketta" Chugaev <riketta@outlook.com>
2023-12-02 21:54:53 +0300

    Update ClassicBot documentation

--- Warcraft Fishman/Bots/ClassicBot.cs
+++ Warcraft Fishman/Bots/ClassicBot.cs
@@ -38,6 +38,7 @@ namespace Fishman
         /// Tips:
         /// - Higher (or sometimes lower) quality water graphics can help increase the success rate of fishing attempts.
         /// - The higher your character is, the more to the right the fishing-line will be (so less overlap with the bobber).
+        /// - Reducing the FPS on weak systems (e.g. with an integrated GPU) to 60-80 may help, and also set the water quality to "Fair".
         /// </summary>
         /// <param name="preset">Actions preset that should be used for fishing.</param>
         /// <param name="options">Set of options for this bot version.</param>
